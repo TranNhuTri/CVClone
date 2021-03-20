e17a99d90d4f8bc51049a103f7a88f12a0d7cf67
Tran Nhu Tri <trannhutri0703@gmail.com>
2021-03-20 15:56:22 +0700

    'about_me'

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-var appHeader = document.getElementById("header");
-var width = (screen.width - appHeader.offsetWidth)/2;
-appHeader.style.right =  width.toString() + "px";
 var contentChange = document.getElementById("textChange");
+
 var pointer = document.getElementById("pointer");
+
 var messages = [], index = 0;
 
 var s1 = "Developer", s2 = "Designer";
+
 for(let i = 0; i <= s1.length; i++)
     messages.push(s1.substring(0, i));
+
 for(let i = 0; i <= s2.length; i++)
     messages.push(s2.substring(0, i));
+
 function cycle() {
     contentChange.innerText = messages[index];
     index++;
@@ -33,3 +35,17 @@ function pointerHide()
     setTimeout(pointerHide, 500);
 }
 pointerHide();
+
+var appHeader = document.getElementById("header");
+window.onscroll = function()
+{
+    var pixel = window.scrollY;
+    if(pixel)
+    {
+        appHeader.classList.add("header--active");
+    }
+    else
+    {
+        appHeader.classList.remove("header--active");
+    }
+}
